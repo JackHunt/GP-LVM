517ba9992bea82f5812c6713a34daec9c1e10629
Jack Hunt <jackmileshunt@gmail.com>
2017-06-10 16:11:11 +0100

    More skeleton stuff

--- src/demo.py
+++ src/demo.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 import linear_gplvm as lgp
+import nonlinear_gplvm as nlgp
 
 if __name__ == "__main__":
-    gplvm = lgp.LinearGPLVM()
-    gplvm.hello_base()
+    gplvm = nlgp.NonlinearGPLVM()
+    gplvm.hello()
---
+++ src/nonlinear_gplvm.py
@@ -0,0 +1,5 @@
+from gplvm import *
+
+class NonlinearGPLVM(GPLVM):
+    def hello(self):
+        print("Hello from nonlinear GPLVM")
