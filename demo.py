6b71dc1a784a2933ac77d5e7a4eeebee6de4563b
Jack Hunt <jackmileshunt@gmail.com>
2017-06-11 13:29:02 +0100

    Added iris to gitignore

--- demo.py
+++ demo.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 import sys
 sys.path.insert(0, './gplvm_lib')
+import urllib.request
+import os.path
 import numpy as np
 import gplvm_lib as gp
 
+irisURL = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+irisFname = 'iris.data'
+
+def getIris():
+    iris = []
+    if not os.path.isfile(irisFname):
+        try:
+            urllib.request.urlretrieve(irisURL, irisFname)
+        except urllib.request.URLError:
+            sys.exit("Unable to download iris dataset. Quitting.")
+    return iris
+
 if __name__ == "__main__":
-    a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    data = getIris()
     gplvm = gp.LinearGPLVM(a + np.eye(3)*5)
     gplvm.compute(2, 2.0)
     #gp.pca(np.array([[1, 2, 3], [1, 2, 3]]), 2, True)
